f1849d569c74441f541f7dd3e0f5fd7582bbfec3
Medy <mannomedy@gmail.com>
2025-08-20 09:25:34 +0300

    add changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+#!/usr/bin/env python3
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower() 
+    def match(self, words):
+        matches = []
+        for candidate in words:
+            if candidate.lower() != self.word:  
+                if sorted(candidate.lower()) == sorted(self.word):
+                    matches.append(candidate)
+        return matches
